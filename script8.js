07c498b6bf90a6a8aa0222ae50c841a5784898e5
Zmulvihill <zmulvihi@yahoo.com>
2023-02-01 17:56:42 -0700

    added script8 and ran node

---
+++ script8.js
@@ -0,0 +1,9 @@
+console.log("// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;")
