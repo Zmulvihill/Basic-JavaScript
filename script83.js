0df4f358f491cd231dcb764e74255f5eb3ecbecf
Zmulvihill <zmulvihi@yahoo.com>
2023-02-08 13:25:22 -0700

    added script83, didn't run node

---
+++ script83.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Tank",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["man", "dogs"]
+  
+    // Only change code above this line
+  };
