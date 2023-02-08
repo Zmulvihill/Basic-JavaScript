922c05694d444cc968e3180937645e409e845c72
Zmulvihill <zmulvihi@yahoo.com>
2023-02-08 15:02:52 -0700

    added script92, didn't run node

---
+++ script92.js
@@ -0,0 +1,16 @@
+const myMusic = [
+    {
+     artist: "Billy Joel",
+     title: "Piano Man",
+     release_year: 1973,
+     formats: ["CD", "8T", "LP"],
+     gold: true
+   },
+   {
+     "artist": "Neffex",
+     "title": "Built to Last",
+     "release_year": 2022,
+     "formats": ["CD", "Spotify", "Download"],
+     "gold": true
+   }
+ ];
