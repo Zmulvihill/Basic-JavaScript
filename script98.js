327adbfa30efc365d386f860052105edc2f48611
Zmulvihill <zmulvihi@yahoo.com>
2023-02-10 10:06:41 -0700

    added script98 and ran node

---
+++ script98.js
@@ -0,0 +1,10 @@
+//Iterate Odd Numbers With a For Loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 11; i += 2) {
+  myArray.push(i);
+}
+var result = myArray[7]
+console.log(result)
