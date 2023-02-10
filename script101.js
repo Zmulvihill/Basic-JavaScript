6420755a2a2998c33c47e6b43f27dca48b3cc8d2
Zmulvihill <zmulvihi@yahoo.com>
2023-02-10 10:56:23 -0700

    added script101 and ran node

---
+++ script101.js
@@ -0,0 +1,16 @@
+//Nesting For Loops
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < arr[i].length; j++) {
+        product = product * arr[i][j];
+      }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+  var result = multiplyAll([[1, 2], [3, 4], [5, 6, 7]])
+  console.log(result)
