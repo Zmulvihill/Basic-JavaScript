49ac23320fe2075318ddb8aa5fed333d8928e9ac
Zmulvihill <zmulvihi@yahoo.com>
2023-02-10 10:02:54 -0700

    added script97 and ran node

---
+++ script97.js
@@ -0,0 +1,10 @@
+//Iterate with JavaScript For Loops continued
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+var result = myArray[6]
+console.log(result)
